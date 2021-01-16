cf2daba93b7cedd82ba02622f2570caab1814e86
AslamHossain <aslamecehstu43java@gmail.com>
2021-01-16 21:47:25 +0600

    URI 1001 accepted

---
+++ URI/src/beginner/URI_1001.java
@@ -0,0 +1,13 @@
+package beginner;
+
+import java.util.Scanner;
+
+public class URI_1001 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int X = A + B;
+		System.out.print("X = " + X + "\n");
+	}
+}
